fb743e07b42eafc495057b4a2a20f9b5fc6bc419
OwnNightmare <yura.borisov.9523@gmail.com>
2021-10-28 15:05:30 +0300

    func rate_by_intel_.. added'

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ class Hero:
         self.name = name
         resp = requests.get(Hero.url + f'search/{self.name}')
         self.hero_js = resp.json()
+        self.intellect = int(self.hero_js['results'][0]['powerstats']['intelligence'])
+
+    def __gt__(self, other):
+        if self.intellect > other.intellect:
+            return True
+
+    def __lt__(self, other):
+        if self.intellect < other.intellect:
+            return False
+
+    def __eq__(self, other):
+        if self.intellect == other.intellect:
+            return True
+
+    def __str__(self):
+        return self.name
 
     def show_hero_js(self):
         pprint (self.hero_js)
@@ -34,21 +50,23 @@ def find_smartest(given_heroes):
     if smartest is not False:
         return f'The most intelligence hero is {smartest} - {points} points'
 
-# Hulk = 'search/Hulk'
-# Captain = 'search/Captain America'
-# Thanos = 'search/Thanos'
-# response = requests.get(url + Thanos)
-# pprint(response.json()['results'][0]['id'])
-# # print(response.json()['results'][0]['powerstats']['intelligence'])
 
-hulk = Hero('Hulk')
+def rate_heroes_by_intellect(heroes):
+    place = 1
+    print('\t Heroes by intelligence:'.upper())
+    for hero in sorted(heroes, key=lambda supe: supe.intellect, reverse=True ):
+        print(f'{place}.{hero.name} - {hero.intellect} points')
+        place += 1
+
+
+Hulk = Hero('Hulk')
+harry = Hero('Harry Potter')
 Thanos = Hero('Thanos')
+Captain = Hero('Captain America')
+my_heroes = [Hulk, Thanos, Captain, harry]
 
-print(find_smartest([hulk, Thanos]))
-# hulk.show_hero_js()
-
-
-
+print(find_smartest(my_heroes))
+rate_heroes_by_intellect(my_heroes)
 
 
 
