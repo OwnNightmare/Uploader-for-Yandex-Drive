5c8c4ba3be6a0ee98099fb157d3a962df08ab731
OwnNightmare <yura.borisov.9523@gmail.com>
2021-10-31 18:30:08 +0300

    Upload from Win and Unix now

--- Uploading.py
+++ Uploading.py
@@ -2,16 +2,18 @@ from YADriveAPI import YaUploader
 from YADriveAPI import Token
 
 
-user_token = input('Введите ваш токен: ')
-file_path = input('Введите путь до файла на компьютере: ')
-drive_path = input('Путь для файла на ЯндексДиске: ')
-split_path = file_path.split('\\')
+os_type = 'unknown'
+while os_type not in ['Win', 'Windows', 'Unix', 'Linux']:
+    os_type = input('Вид вашей ОС ("Win" - Windows, "Unix"- Unix, Linux): ').capitalize()
+file_path = input('Путь до файла на компьютере: ')
+if os_type in ['Win', 'Windows']:
+    split_path = file_path.split('\\')
+else:
+    split_path = file_path.split('/')
 file_name = split_path[-1]
-print(drive_path)
-print(file_name)
+user_token = input('Yandex токен: ')
+drive_path = input('Путь для файла на ЯндексДиске: ')
 
-uploader = YaUploader(token=Token)
-uploader.upload_file_to_drive(f'{drive_path}/{file_name}', f'{file_path}')
-
-# ya.upload_file_to_drive('Education/type.txt', 'typ.txt')
-# print('Hi')
+uploader = YaUploader(token=user_token)
+response = uploader.upload_file_to_drive(f'{drive_path}/{file_name}', f'{file_path}')
+print(response)
